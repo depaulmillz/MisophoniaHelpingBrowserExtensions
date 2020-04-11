dd0a4a68491e9b6a2c278885a06aaea051b77bb4
depaulmillz <depaulmillz@users.noreply.github.com>
2020-04-11 01:59:36 -0400

    Updated to a user modifiable app

--- Chrome/NetflixMute/contentScript.js
+++ Chrome/NetflixMute/contentScript.js
@@ -1,11 +1,20 @@
+var toMatch = null;
 
 var callback = function (mutationlist, observer) {
   var process = function () {
-    var docBody = document.body.innerHTML.toLowerCase();
-    var whistleFound = docBody.search("whistl") >= 0;
-    var cheeringFound = docBody.search("cheer") >= 0;
-    var slurpFound = docBody.search("slurp") >= 0;
-    return whistleFound || cheeringFound || slurpFound;
+    var docBody = document.getElementById("trackingForMute").innerText.toLowerCase();
+    //var whistleFound = docBody.search("whistl") >= 0;
+    //var cheeringFound = docBody.search("cheer") >= 0;
+    //var slurpFound = docBody.search("slurp") >= 0;
+    if (toMatch) {
+      for (i = 0; i < toMatch.length; i++) {
+        if (docBody.search(toMatch[i]) >= 0) {
+          //console.log("Matched " + toMatch[i]);
+          return true;
+        }
+      }
+    }
+    return false;
   };
   if (process()) {
     var videoArray = document.getElementsByTagName("video");
@@ -21,6 +30,12 @@ var callback = function (mutationlist, observer) {
 };
 
 var interval = setInterval(function () {
+  chrome.storage.local.get(['tomute'], function (result) {
+    //console.log(result.tomute);
+    if (result.tomute != null && result.tomute != undefined) {
+      toMatch = result.tomute.split("\t");
+    }
+  });
   if (window.location.pathname.search("watch") >= 0) {
     if (document.getElementsByClassName("player-timedtext")[0] != undefined) {
       document.getElementsByClassName("player-timedtext")[0].setAttribute("id", "trackingForMute");
