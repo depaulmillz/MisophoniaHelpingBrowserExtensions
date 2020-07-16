c14c6f51c9fa9d652519b0067fb1833693b020bc
dePaul Miller <dsm220@lehigh.edu>
2020-07-15 20:37:33 -0400

    Updated disney plus muter

--- Chrome/DisneyPlusMute/contentScript.js
+++ Chrome/DisneyPlusMute/contentScript.js
@@ -7,45 +7,60 @@ chrome.storage.local.get(['tomute'], function(result) {
   }
 });
 
+var injected = false;
+
 var interval = setInterval(function() {
   if (window.location.pathname.search('video') >= 0) {
     if (document.getElementsByTagName('video') != undefined &&
         document.getElementsByTagName('video')[0] != undefined) {
       const targetNode = document.getElementsByTagName('video')[0];
 
-      var match = false;
-      var docBody = '';
 
       try {
-        docBody = targetNode.textTracks[0].activeCues[0].text.toLowerCase();
-      } catch (err) {
-        docBody = 'Unable to get textTracks';
-      }
+        targetNode.textTracks[0].oncuechange = function() {
+          var match = false;
 
-      console.log(docBody);
-      console.log(toMatch);
-
-      if (toMatch) {
-        for (i = 0; i < toMatch.length; i++) {
-          if (docBody.search(toMatch[i]) >= 0) {
-            match = true;
-            break;
-          }
-        }
-      }
-
-      if (match) {
-        var videoArray = document.getElementsByTagName('video');
-        if (videoArray != undefined) {
-          var videoZero = videoArray[0];
-          if (videoZero !== undefined) {
-            if (videoZero.muted !== undefined) {
-              console.log('Muted');
-              videoZero.muted = true;
+          try {
+            var docBody =
+                targetNode.textTracks[0].activeCues[0].text.toLowerCase();
+            if (toMatch) {
+              for (i = 0; i < toMatch.length; i++) {
+                if (docBody.search(toMatch[i]) >= 0) {
+                  match = true;
+                  break;
+                }
+              }
+              if (match) {
+                var videoArray = document.getElementsByTagName('video');
+                if (videoArray != undefined) {
+                  var videoZero = videoArray[0];
+                  if (videoZero !== undefined) {
+                    if (videoZero.muted !== undefined) {
+                      console.log('Muted');
+                      videoZero.muted = true;
+                    }
+                  }
+                }
+              }
             }
+          } catch (e) {
           }
-        }
+        };
+        injected = true;
+      } catch (err) {
+        console.log('Unable to get textTracks');
+        injected = false;
       }
+
+      if (injected) {
+        console.log('injected');
+        clearInterval(interval);
+      }
+
+      // activeCues[0].text.toLowerCase();
+
+      // console.log(docBody);
+      // console.log(toMatch);
     }
   }
 }, 500);
